8da9afd48573e2dbb9f50c7acbe23f830659d8a6
yangmw7 <yangmw7@naver.com>
2025-05-04 01:56:01 +0900

    feat: 백엔드 기본 세팅 및 React 연동 테스트 완료

---
+++ src/main/java/com/example/game_backend/GameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.game_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameBackendApplication.class, args);
+	}
+
+}
