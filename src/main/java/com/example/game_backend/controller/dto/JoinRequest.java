2e2ef1f7a568f94f452d36aacec1cd36aa7e1112
yangmw7 <yangmw7@gmail.com>
2025-05-06 16:31:44 +0900

    feat: mustache 기반 로그인/회원가입/로그아웃 UI 연동 및 테스트 완료

--- src/main/java/com/example/game_backend/controller/dto/JoinRequest.java
+++ src/main/java/com/example/game_backend/controller/dto/JoinRequest.java
@@ -1,13 +1,14 @@
 package com.example.game_backend.controller.dto;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@Getter
+@NoArgsConstructor            // 기본 생성자
+@AllArgsConstructor           // 모든 필드를 받는 생성자
 public class JoinRequest {
-
     private String username;
-
     private String password;
-
     private String email;
 }
