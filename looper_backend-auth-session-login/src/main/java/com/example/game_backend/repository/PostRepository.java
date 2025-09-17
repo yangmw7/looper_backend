bfa299b8167ae51f448203b64328833c306641b8
yangmw7 <yangmw7@gmail.com>
2025-09-18 01:22:01 +0900

    chore: remove mistakenly added looper_backend-auth-session-login folder

--- looper_backend-auth-session-login/src/main/java/com/example/game_backend/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.game_backend.repository;
-
-import com.example.game_backend.repository.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
--- looper_backend-auth-session-login/src/main/java/com/example/game_backend/security/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.game_backend.security;
-
-
-public enum Role {
-    USER,   // 일반 사용자
-    ADMIN   // 관리자
-}
