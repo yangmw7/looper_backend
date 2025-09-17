73a9f007e1e1af8d231df5769427f66bd5fb2027
yangmw7 <yangmw7@naver.com>
2025-09-18 01:22:01 +0900

    chore: remove mistakenly added looper_backend-auth-session-login folder

--- looper_backend-auth-session-login/src/main/java/com/example/game_backend/service/PasswordService.java
+++
@@ -1,8 +0,0 @@
-package com.example.game_backend.service;
-
-import com.example.game_backend.controller.dto.ResetPasswordRequest;
-
-public interface PasswordService {
-    boolean verifyUser(ResetPasswordRequest request);
-    void resetPassword(String username, String newPassword);
-}
