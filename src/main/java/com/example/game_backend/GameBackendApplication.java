d691d3f94a92d1e91888bcee8f1531aa0c2f1bbc
yangmw7 <yangmw7@naver.com>
2025-10-18 18:23:03 +0900

    타임존 업데이트

--- src/main/java/com/example/game_backend/GameBackendApplication.java
+++ src/main/java/com/example/game_backend/GameBackendApplication.java
@@ -2,11 +2,21 @@ package com.example.game_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing; // ✅ 이거 import도 필요
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing  //  @CreatedDate, @LastModifiedDate
 public class GameBackendApplication {
+
+	@PostConstruct
+	public void init() {
+		// JVM 전체의 기본 타임존을 Asia/Seoul로 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GameBackendApplication.class, args);
 	}
